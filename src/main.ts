ec4db9243e4c5a9115caf7aefaacdbce61aa57cb
GuilhermeSzFernandes <guilhermenovo3001@gmail.com>
2026-03-01 16:39:09 -0300

    feat: add TikTok site verification and configure static file serving

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,13 @@ import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
 import { PrismaClientExceptionFilter } from 'nestjs-prisma';
 import { HttpStatus } from '@nestjs/common';
+import * as express from 'express';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+   app.use('/.well-known', express.static(join(__dirname, '..', 'public')));
   const { httpAdapter } = app.get(HttpAdapterHost);
   app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter, {
     P2000: HttpStatus.BAD_REQUEST,
